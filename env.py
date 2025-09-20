1a71864edb52fc357c94ec00e4062ed4480abf2a
Erik Cook <cookie2004@gmail.com>
2025-09-20 10:32:01 -0400

    250920

--- env.py
+++ env.py
@@ -1,10 +1,10 @@
 #Program directories
-HMMER_BIN = "./hmmer/bin" #Bin directory of hmmer ex. /home/ubuntu/programs/hmmer/bin
-CLUSTAL_O = "./clustalo"  #Directory of clustalo executable ex. /home/ubuntu/programs/clustalo
+HMMER_BIN = "/home/ubuntu/cookstore/FIDO/hmmer/bin" #Bin directory of hmmer ex. /home/ubuntu/programs/hmmer/bin
+CLUSTAL_O = "/home/ubuntu/cookstore/FIDO/clustalo"  #Directory of clustalo executable ex. /home/ubuntu/programs/clustalo
 
 #MSA Processing
-MIN_SEQ_LEN = 0
-MAX_SEQ_LEN = 600
+MIN_SEQ_LEN = 10
+MAX_SEQ_LEN = 10000
 
 MIN_IDEN = 30
 MAX_IDEN = 101 #Set greater than 100 to include identical sequences.
